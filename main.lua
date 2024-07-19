c603df24e11f831be67ea243f7151453a106c89a
Nama Anda <dakotaalexander74@gmail.com>
2024-07-19 19:28:53 +0700

    Unfinish Lua File

---
+++ main.lua
@@ -0,0 +1,9 @@
+print("aku keren")
+
+-- function love.load()
+--     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+--         fullscreen = false,
+--         resiazble = false,
+--         vsync = true
+--     })
+-- end
